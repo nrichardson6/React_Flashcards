d210e8e57529c2085f3d3ebb3d4c8442d96dd528
Nick Richardson <nickrichardson337@gmail.com>
2020-09-16 12:18:47 -0600

    stuck

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Container, Header, Button, Icon, Modal } from "semantic-ui-react"; 
+import Flashcards from "./Flashcards";
+import FlashcardForm from "./FlashcardForm";
+import Flashcards2 from "./Flashcards2"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+state = {
+  flashcards: [
+    { id: 1, question: "What is the Matrix?", answer: "follow the white rabbit", },
+    { id: 2, question: "What is the meaning of life", answer: "The answer is 42", },
+    { id: 3, question: "Question this", answer: "Still trying to figure this out...", },
+  ]
+}
+
+addFlashcard = (flashcard) => {
+  console.log('Add :>> ', flashcard);
+  const newFlashcard = { ...flashcard, id: (Math.random()*100)}
+  this.setState({
+    flashcards: [newFlashcard, ...this.state.flashcards],
+  })
+}
+
+  render() {
+    return (
+      <Container>
+        <Header as="h1">Flash Cards</Header>
+        <FlashcardForm addFlashcard_via_FlashcardForm={this.addFlashcard} />
+
+        <Flashcards2 flashcards={this.state.flashcards} />
+       
+      </Container>
+    );
+  }
+  debugger;
 }
 
 export default App;
---
+++ src/flashcards2.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Button, Header, Icon, Modal } from 'semantic-ui-react'
+
+const Flashcards2 = ({flashcards}) => {
+  const [open, setOpen] = React.useState(false)
+  return flashcards.map(card => 
+    (
+    <Modal
+      id={card.id}
+      basic
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      size='small'
+      trigger={<Button id={card.id}>{card.question}</Button>}
+      
+      
+    >
+      <Header icon>
+        <Icon name='flag checkered' />
+        Answer
+      </Header>
+      <Modal.Content>
+        <p>
+          {card.answer}
+        </p>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color='green' inverted onClick={() => setOpen(false)}>
+          <Icon name='checkmark' /> Ok
+        </Button>
+        <Button color='red' inverted onClick={() => setOpen(false)}>
+          <Icon name='trash alternate outline icon' /> Delete
+        </Button>
+      </Modal.Actions>
+    </Modal>
+
+  )
+  
+  
+  )
+ 
+}
+
+
+export default Flashcards2;
